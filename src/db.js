7d0cc40a65bb8f46c072dd032438470f0cda11ea
German <german2717@gmail.com>
2023-08-13 21:54:07 -0300

    listo

--- src/db.js
+++ src/db.js
@@ -2,15 +2,29 @@ require("dotenv").config();
 require("pg");
 
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, BDD } = process.env;
-console.log(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, BDD);
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${BDD}`,
-  {
-    logging: console.log, // set to console.log to see the raw SQL queries
-    native: false,
-  }
-);
+
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, BDD } = process.env;
+// console.log(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, BDD);
+const { DB_DEPLOY } = process.env;
+
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${BDD}`,
+//   {
+//     logging: console.log, // set to console.log to see the raw SQL queries
+//     native: false,
+//   }
+// );
+
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Solo para desarrollo local. En producción, configúralo correctamente.
+    },
+  },
+});
 
 // Importa los modelos aquí y configúralos si es necesario
 const PrecioModel = require("./models/price")(sequelize, Sequelize);
